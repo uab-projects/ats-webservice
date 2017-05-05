54b61a5c428c4bb97a3c15c5fa52754907c08576
David LJ <davidlj@smartlysoft.com>
2017-05-05 12:05:10 +0200

    Added POST self response and print

--- src/main/java/codes/uab/ats/webservice/MyResource.java
+++ src/main/java/codes/uab/ats/webservice/MyResource.java
@@ -1,9 +1,12 @@
 package codes.uab.ats.webservice;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -21,5 +24,12 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Hello, Heroku!";
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response post(String req) {
+    	System.out.println(req);
+    	return Response.status(200).entity(req).build();
     }
 }
